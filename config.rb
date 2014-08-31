13d5aba4a2547eb9d653190fd84f8953b5947b0f
oncomouse <oncomouse@gmail.com>
2014-08-31 10:01:15 -0700

    Switch compass to preferring SCSS syntax.

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "stylesheets/sass"
+sass_dir = "stylesheets/scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
@@ -18,6 +18,6 @@ javascripts_dir = "javascripts"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-preferred_syntax = :sass
+#preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
